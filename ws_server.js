1151c001fd88c9374b7093377b624a8be7f7b9e8
Jose David <david_gh86@hotmail.es>
2021-12-18 20:59:41 +0100

    heartbeat

--- ws_server.js
+++ ws_server.js
@@ -5,11 +5,31 @@ const wsServer = new ws.Server({ noServer: true });
 
 const eventEmitter = new EventEmitter()
 
+function heartbeat() {
+  this.isAlive = true;
+}
+
 wsServer.on('connection', function connection(ws) {
+    ws.isAlive = true;
+    ws.on('pong', heartbeat)
     eventEmitter.on('send_ws_message', function (text) {
         ws.send(text)
     })
 });
+
+const interval = setInterval(function ping() {
+    wsServer.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 30000);
+
+wsServer.on('close', function close() {
+    clearInterval(interval);
+});
+  
 
 wsServer.send = function (message) {
     eventEmitter.emit('send_ws_message', message)
