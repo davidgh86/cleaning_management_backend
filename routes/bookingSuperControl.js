c44864585a4e5187b789adafa2cb7b338336f5b1
Jose David <david_gh86@hotmail.es>
2022-01-17 20:25:44 +0100

    save values

--- routes/bookingSuperControl.js
+++ routes/bookingSuperControl.js
@@ -34,13 +34,14 @@ const storage = multer.diskStorage({
     }
   })
 
-schedule.scheduleJob('* 23 * * *', function(){
+schedule.scheduleJob('* 23 * * *', async function(){
     let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
     let date = new Date(Date.now())
     date.setDate(date.getDate() + 1)
     date = getStartOfDateFromEpoch(parseInt(date/1), timezone)
     let fileName = date.getTime() + ".csv"
     download(new Date(Date.now()), new Date(Date.now()), "andres@dublincityapartments.ie", "Briego912Celeste:)", saveRootPath+"/"+fileName)
+    await updateValuesInMemory(date, timezone)
 });
 
 const upload = multer({ storage })
